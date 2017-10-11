218d7558b872643cb4abba23b8687d109681b6c5
dankinsoid <voidilov@gmail.com>
2017-10-11 21:42:12 +0300

    Initial Commit

---
+++ ExchangeApi/ExchangeApi.h
@@ -0,0 +1,13 @@
+//
+//  ExchangeApi.h
+//  ExchangeApi
+//
+//  Created by Данил Войдилов on 11.10.17.
+//  Copyright © 2017 Данил Войдилов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExchangeApi : NSObject
+
+@end
---
+++ ExchangeApi/ExchangeApi.m
@@ -0,0 +1,13 @@
+//
+//  ExchangeApi.m
+//  ExchangeApi
+//
+//  Created by Данил Войдилов on 11.10.17.
+//  Copyright © 2017 Данил Войдилов. All rights reserved.
+//
+
+#import "ExchangeApi.h"
+
+@implementation ExchangeApi
+
+@end
